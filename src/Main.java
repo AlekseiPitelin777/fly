dcd18a0eb6089a8a8e7db794859e0a74d9bf6c52
pitelinalexey <pitelinalexey@mail.ru>
2020-10-03 00:38:43 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+
+import com.skillbox.airport.Aircraft;
+import com.skillbox.airport.Airport;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Main {
+    public static void main(String[]args){
+        Airport airport = Airport.getInstance();
+       System.out.println(airport.getAllAircrafts());
+
+
+            List<Aircraft> aircraftList = new ArrayList();
+            System.out.println(aircraftList.size());
+
+
+
+
+
+
+
+
+
+
+    }
+}
