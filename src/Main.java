496724d375651c217d0db485d7482e33cae3f832
pitelinalexey <pitelinalexey@mail.ru>
2020-10-03 15:17:02 +0300

    count planes

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,15 @@
 
-import com.skillbox.airport.Aircraft;
+
 import com.skillbox.airport.Airport;
 
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class Main {
     public static void main(String[]args){
         Airport airport = Airport.getInstance();
        System.out.println(airport.getAllAircrafts());
-
-
-            List<Aircraft> aircraftList = new ArrayList();
-            System.out.println(aircraftList.size());
-
-
-
+        airport.getAllAircrafts().size();
+        System.out.println(airport.getAllAircrafts().size());
 
 
 
